322323dfbca5829dd97965f5660c53792f891dea
DeoEsor <RulianValeriy@yandex.ru>
2022-12-02 18:01:27 +0300

    added FileDownloadActor

---
+++ Gefest.Actors/FileDownload/FileDownloadActor.cs
@@ -0,0 +1,79 @@
+﻿using Akka.Actor;
+using Gefest.Actors.Extensions;
+using Gefest.Contracts;
+using Gefest.Domain.Messages.Download;
+
+namespace Gefest.Actors.FileDownload;
+
+public class FileDownloadActor 
+    : ReceiveActor, IRequestHandler<DownloadRequestMessage>
+{
+    #region State
+
+    private Uri? _currentUri;
+    private string? _currentTargetPath;
+    private IActorRef? _currentDownloadRequester;
+    
+    public IStash? Stash { get; set; } = null;
+
+
+    #endregion
+    
+    #region Initialization
+
+    public FileDownloadActor()
+    {
+        Become(Ready);
+    }
+
+    #endregion
+    
+    #region States
+
+    public void Ready()
+    {
+        ReceiveAsync<DownloadRequestMessage>(Handler);
+    }
+
+    public void Downloading()
+    {
+        Receive<DownloadRequestMessage>(_ => Stash?.Stash());
+        Receive<DownloadCompletedMessage>(_ => 
+        {
+            Become(Ready);
+            Stash?.UnstashAll();
+        });
+    }
+
+    #endregion
+    
+    #region Handlers
+    
+    private async Task Handler(DownloadRequestMessage message)
+    {
+        await HandleRequest(message);
+        Become(Downloading);
+    }
+
+    public async Task HandleRequest(DownloadRequestMessage request)
+    {
+        _currentUri = request.Uri;
+        _currentTargetPath = Path.GetTempFileName();
+        _currentDownloadRequester = Sender;
+
+        _currentDownloadRequester.Tell(new DownloadStartedMessage(_currentUri));
+        
+        await StartDownloadAsync();
+    }
+    
+    private async Task StartDownloadAsync()
+    {
+        using var client = new HttpClient();
+        await client.DownloadLargeFile("", "");
+
+        _currentDownloadRequester.Tell(DownloadCompletedMessage.CreateMessage(_currentUri, _currentTargetPath));
+        Become(Ready);
+    }
+
+    #endregion
+}
