a7906f6edd54365028b7c18c16b32a9b452e90d0
DeoEsor <RulianValeriy@yandex.ru>
2022-12-08 16:38:48 +0300

    refactored FileDownloadActor.cs

--- Gefest.Actors/FileDownload/FileDownloadActor.cs
+++ Gefest.Actors/FileDownload/FileDownloadActor.cs
@@ -1,54 +1,37 @@
 ﻿using Akka.Actor;
 using Gefest.Actors.Extensions;
 using Gefest.Contracts;
-using Gefest.Domain.Messages.Download;
+using Gefest.Domain.Messages;
 
 namespace Gefest.Actors.FileDownload;
 
 public class FileDownloadActor 
     : ReceiveActor, IRequestHandler<DownloadRequestMessage>
 {
-    #region State
-
     private Uri? _currentUri;
     private string? _currentTargetPath;
     private IActorRef? _currentDownloadRequester;
     
     public IStash? Stash { get; set; } = null;
-
-
-    #endregion
     
-    #region Initialization
+    public FileDownloadActor() =>Become(Ready);
 
-    public FileDownloadActor()
-    {
-        Become(Ready);
-    }
-
-    #endregion
-    
-    #region States
-
-    public void Ready()
-    {
-        ReceiveAsync<DownloadRequestMessage>(Handler);
-    }
+    public void Ready() => ReceiveAsync<DownloadRequestMessage>(Handler);
 
     public void Downloading()
     {
-        Receive<DownloadRequestMessage>(_ => Stash?.Stash());
-        Receive<DownloadCompletedMessage>(_ => 
-        {
-            Become(Ready);
-            Stash?.UnstashAll();
-        });
+        Receive<DownloadRequestMessage>(OnRequest);
+        Receive<DownloadCompletedMessage>(OnCompleteMsg);
     }
 
-    #endregion
-    
-    #region Handlers
-    
+    private void OnCompleteMsg(DownloadCompletedMessage _)
+    {
+        Become(Ready);
+        Stash?.UnstashAll();
+    }
+
+    private void OnRequest(DownloadRequestMessage _) => Stash?.Stash();
+
     private async Task Handler(DownloadRequestMessage message)
     {
         await HandleRequest(message);
@@ -71,9 +54,7 @@ public class FileDownloadActor
         using var client = new HttpClient();
         await client.DownloadLargeFile(_currentUri?.AbsoluteUri!, _currentTargetPath!);
 
-        _currentDownloadRequester.Tell(DownloadCompletedMessage.CreateMessage(_currentUri, _currentTargetPath));
+        _currentDownloadRequester.Tell(new DownloadCompletedMessage(_currentUri, _currentTargetPath));
         Become(Ready);
     }
-
-    #endregion
 }
